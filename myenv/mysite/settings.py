d8f7a7f21c9426225052e243398c8f581f48f26a
ktkhshi <ktkhshi@gmail.com>
2024-04-17 22:14:06 +0900

    Django prepare

--- myenv/mysite/settings.py
+++ myenv/mysite/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "djoser",
     "accounts",
+    "app", # 追加
     "cloudinary",
     "cloudinary_storage",
     "corsheaders",
--- myenv/mysite/urls.py
+++ myenv/mysite/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path("api/auth/", include("djoser.urls.jwt")),
     # アカウント
     path("api/", include("accounts.urls")),
+    # 投稿
+    path("api/", include("app.urls")),
     # 管理画面
     path("admin/", admin.site.urls),
 ]
